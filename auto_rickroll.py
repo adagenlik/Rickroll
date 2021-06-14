86457959336f7d9f52b8b1044197fa2efa769c8c
adagenlik <64929407+adagenlik@users.noreply.github.com>
2021-06-14 23:15:34 +0300

    Add files via upload

---
+++ auto_rickroll.py
@@ -0,0 +1,20 @@
+import pyautogui
+
+pyautogui.press("win")
+pyautogui.typewrite("chrome")
+pyautogui.press("enter")
+pyautogui.sleep(2)
+pyautogui.typewrite("youtube.com")
+pyautogui.press("enter")
+pyautogui.sleep(4)
+pyautogui.press("tab")
+pyautogui.press("tab")
+pyautogui.press("tab")
+pyautogui.press("tab")
+pyautogui.typewrite("Never Gonna Give You Up")
+pyautogui.press("enter")
+pyautogui.sleep(2)
+pyautogui.press("tab")
+pyautogui.press("enter")
+
+
